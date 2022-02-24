7904f705ad3eeabff626aed357c1e598c1e50c4e
orlovinc <i.n.orlov@yandex.ru>
2022-02-24 14:03:57 +0300

    1

---
+++ zadanie2/two/two/two.cpp
@@ -0,0 +1,66 @@
+﻿
+
+#include <iostream>
+#include <stack>
+
+
+class Stack
+{
+public:
+    int last_index;
+    int size;
+    int* array;
+
+    Stack(int new_size)
+    {
+        size = new_size;
+        array = new int[size];
+        last_index = 0;
+    }
+    ~Stack()
+    {
+        delete[] array;
+    }
+    void Show()
+    {
+        std::cout << last_index;
+    }
+    void push(int new_elem)
+    {
+        if (last_index < size)
+        {
+            array[last_index] = new_elem;
+            last_index = last_index + 1;
+            Show();
+        }
+    }
+    int pop()
+    {
+        if (last_index > 0)
+        {
+            last_index = last_index - 1;
+            return array[last_index];
+            Show();
+        }
+    }
+
+};
+
+
+int main()
+{
+    Stack s(4);
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.pop();
+
+};
+
+//К сожалению мне не хватает знаний чтобы выполнить это задание по всем требованиям.
+//Я ознакомился с материалами  о том, что из себя должен представлять циклический буфер.
+//В моем понимании это что - то вроде структуры данных Стек, которая работает по принципу "последний вошел, первый вышел".
+//Я скорее всего ошибаюсь и представленный мной код лишь попытка показать, хоть какие - то навыки, по работе с памятью,
+//по этому могу представить только один класс вашему вниманию и сравнивать конечно не с чем.
+
---
+++ zadanie3/sort/sort/sort.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+using namespace std;
+
+int main() {
+
+    int sort_int_array[] = { 7,10,11,5,8,3,53 };
+    vector<int> sort_int_vector(sort_int_array, sort_int_array + 7);
+    int i;
+    sort(sort_int_vector.begin(), sort_int_vector.end());
+    for (i = 0; i < 7; i++)
+        cout << sort_int_vector[i] << " ";
+    cout << "\n";
+}
+
+// Для сортировки массива чисел(в моем случае по возрастанию), я использовал функцию std::sort, которая находится в библиотеке algorithm.
+// Мой выбор пал на данную функцию т.к. она чаще всего опережает по скорости qshort. Но вообще существует множество функций сортировок(например алгоритм introsort который переключается на пирамидальную сортировку),
+// но каждая из них будет эффективна в зависиммости от поставленной задачи.
